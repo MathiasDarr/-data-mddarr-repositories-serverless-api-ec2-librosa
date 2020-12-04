b0c30c48218dc8de88a01f38e21dbdc19b1767c4
mddarr <mddarr@gmail.com>
2020-12-04 13:18:51 -0800

    implement test on the presigned post

--- tests/test_presigned_url_sqs_event.py
+++ tests/test_presigned_url_sqs_event.py
@@ -1,3 +1,8 @@
+"""
+This file contains tests of the
+"""
+
+import pytest
 import logging
 import boto3
 from botocore.exceptions import ClientError
@@ -5,12 +10,11 @@ import json
 import os
 import requests
 
-BUCKET = 'dakobed-sqs-transform-bucket'
 
 def create_presigned_post(bucket_name, object_name, fields=None, conditions=None, expiration=3600):
     # Generate a presigned S3 POST URL
     s3_client = boto3.client('s3')
-    s3_resource= boto3.resource('s3')
+    s3_resource = boto3.resource('s3')
     try:
         response = s3_client.generate_presigned_post(bucket_name,
                                                      object_name,
@@ -23,14 +27,24 @@ def create_presigned_post(bucket_name, object_name, fields=None, conditions=None
 
     # The response contains the presigned URL and required fields
     return response
-fileName = 'dakobedbard/jazz3_solo.wav'
-response = create_presigned_post(BUCKET, fileName)
 
-fields = response['fields']
-access_key = fields['AWSAccessKeyId']
 
-with open(fileName, 'rb') as f:
-    files = {'file': (fileName, f)}
-    http_response = requests.post(response['url'], data=response['fields'], files=files)
+def upload_file_with_presigned_url():
+    """
+    This function uses the presigned url returned and uploads to s3.
+    :return: requests.response
+    """
+    fileName = 'jazz3_solo.wav'
+    filePath = '{}/{}'.format('dakobedbard', fileName)
+    BUCKET = 'dakobed-sqs-transform-bucket'
+    response = create_presigned_post(BUCKET, filePath)
 
-req = http_response.request
+    with open(fileName, 'rb') as f:
+        files = {'file': (filePath, f)}
+        http_response = requests.post(response['url'], data=response['fields'], files=files)
+    return http_response
+
+
+def test_upload_presigned_url():
+    http_response = upload_file_with_presigned_url()
+    assert http_response.status_code == 204
